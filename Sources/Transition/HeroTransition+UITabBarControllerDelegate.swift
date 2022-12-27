109a6a3e488ee800388d18e1db8853e8870f773c
zabaluevt <zabti@yandex.ru>
2022-12-27 12:55:14 +0400

    fix tabbarcontroller

--- Sources/Transition/HeroTransition+UITabBarControllerDelegate.swift
+++ Sources/Transition/HeroTransition+UITabBarControllerDelegate.swift
@@ -42,9 +42,11 @@ extension HeroTransition: UITabBarControllerDelegate {
   public func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
     guard !isTransitioning else { return nil }
     self.state = .notified
-    let fromVCIndex = tabBarController.children.firstIndex(of: fromVC)!
-    let toVCIndex = tabBarController.children.firstIndex(of: toVC)!
-    self.isPresenting = toVCIndex > fromVCIndex
+    let fromVCIndex = tabBarController.children.firstIndex(of: fromVC)
+    let toVCIndex = tabBarController.children.firstIndex(of: toVC)
+    if let fromVCIndex = fromVCIndex, let toVCIndex = toVCIndex {
+      self.isPresenting = toVCIndex > fromVCIndex
+    }
     self.fromViewController = fromViewController ?? fromVC
     self.toViewController = toViewController ?? toVC
     self.inTabBarController = true
